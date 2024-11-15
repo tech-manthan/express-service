4456e651601b819c7d2c20d42f33c0e1cd1fc0eb
Manthan Sharma <techmanthansharma@gmail.com>
2024-11-15 17:46:02 +0530

    feat(home): add /home endpoint

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,17 @@ import express from "express";
 import CONFIG from "./config";
 import injectDependencies from "./ioc";
 import { globalErrorHandler } from "./middlewares";
+import { Request, Response } from "express";
 
 const app = express();
 
 if (CONFIG.NODE_ENV === "test") {
   injectDependencies();
 }
+
+app.get("/home", (req: Request, res: Response) => {
+  res.send("Welcome to Service Template");
+});
 
 app.use(globalErrorHandler);
 
